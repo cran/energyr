6f5b1f2d65e1d576c4788746d8d7a5b23d5d52c1
Paul Govan <pgovan1@aggienetwork.com>
2016-09-24 12:53:53 +0000

    version 0.1.1

--- R/lng.R
+++ R/lng.R
@@ -2,16 +2,17 @@
 #'
 #' LNG Plant Information as reported by FERC.
 #'
+#' @import RgoogleMaps
 #' @source http://www.ferc.gov/industries/gas/indus-act/lng.asp
 #' @format Data frame with columns
 #' \describe{
-#' \item{lon}{Approximate Longitude of the Plant}
-#' \item{lat}{Approximate Latitude of the Plant}
-#' \item{Location}{Location of the Plant}
-#' \item{Company}{Name of the Company}
+#' \item{Location}{Location of the LNG Plant}
+#' \item{Company}{Name of the LNG Company}
 #' \item{Capacity}{Capacity of the Plant in Bcfd}
-#' \item{Type}{Type of Plant}
+#' \item{Type}{Type of LNG Plant}
 #' \item{Status}{Status of Plant}
+#' \item{lat}{Approximate Latitude of the Plant}
+#' \item{lon}{Approximate Longitude of the Plant}
 #' }
 #' @examples
 #'   dim(lng)
